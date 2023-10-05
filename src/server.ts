d9f0d6afda5bdc80b88d32fb1253a52a490c56bf
GerritHoskins <gerrit.hoskins@gmail.com>
2023-10-05 11:19:07 +0200

    update webhook and error handling

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import router from './auth/route';
 import { adminAuth, userAuth } from './auth/auth';
 import { verify_signature } from './config/webhooks';
 import { Env, getEnv } from './config/env';
+import { exec } from 'child_process';
 
 const app: Express = express();
 const PORT: string = getEnv(Env.PORT);
@@ -17,12 +18,22 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.post(getEnv(Env.WEBHOOK_GITHUB_SYNC_URL), (req, res) => {
+app.post(getEnv(Env.WEBHOOK_GITHUB_SYNC_URL), (req: Request, res: Response) => {
   if (!verify_signature(req)) {
     res.status(401).send('Unauthorized');
     return;
   }
-  res.status(200).send('Received!');
+  exec('git pull', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send('Failed to pull from git');
+    }
+    if (stderr) {
+      console.warn(`git warnings: ${stderr}`);
+    }
+    console.log(`stdout: ${stdout}`);
+    res.status(200).send('Received and processed!');
+  });
 });
 app.get('/', (req: Request, res: Response) => {
   console.log('Server up and running.');
@@ -61,11 +72,11 @@ const startServer = async () => {
 
 startServer();
 
-process.on('unhandledRejection', (err: unknown) => {
+process.on('uncaughtException', (err: unknown) => {
   if (err instanceof Error) {
-    console.log(`An error occurred: ${err.message}`);
+    console.log(`An uncaught exception occurred: ${err.message}`);
   } else {
-    console.log(`An unexpected rejection occurred: ${err}`);
+    console.log(`An unexpected exception occurred: ${err}`);
   }
   process.exit(1);
 });
