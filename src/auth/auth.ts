89c446913831a3fd8f746521da4571b98db5dab2
GerritHoskins <gerrit.hoskins@gmail.com>
2023-10-05 11:24:03 +0200

    auth: update login + register

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -11,78 +11,49 @@ const jwtSecretLive = getEnv(Env.JWT_SECRET_LIVE);
 
 export const register = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  if (password && password.length < 6) {
-    return res.status(400).json({ message: 'Password less than 6 characters' });
+  if (!password || password.length < 6) {
+    return res.status(400).json({ message: 'Password must be at least 6 characters' });
   }
+
   try {
-    hash(password, 10).then(async (hash: string) => {
-      await User.create({
-        username,
-        password: hash,
-      })
-        .then((user) => {
-          const maxAge = 3 * 60 * 60;
-          const token = sign({ id: user._id, username, role: user.role }, jwtSecretLive, {
-            expiresIn: maxAge, // 3hrs in sec
-          });
-          res.cookie('jwt', token, {
-            httpOnly: true,
-            maxAge: maxAge * 1000, // 3hrs in ms
-          });
-          res.status(201).json({
-            message: 'User successfully created',
-            user: user._id,
-          });
-        })
-        .catch((error) =>
-          res.status(400).json({
-            message: 'User not successful created',
-            error: error.message,
-          }),
-        );
+    const hashedPassword = await hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+    const maxAge = 3 * 60 * 60;
+    const token = sign({ id: user._id, username, role: user.role }, jwtSecretLive, {
+      expiresIn: maxAge,
     });
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ message: 'User successfully created', user: user._id });
   } catch (error) {
     console.error(error);
-    return res.status(400).json({ message: 'User creation failed' });
+    res.status(400).json({ message: 'User creation failed', error: error.message });
   }
 };
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
+
   if (!username || !password) {
     return res.status(400).json({ message: 'Username or Password not provided' });
   }
+
   try {
-    await User.findOne({ username }).then((user) => {
-      if (user?.password) {
-        compare(password, user?.password).then((result: boolean) => {
-          if (result) {
-            const maxAge = 3 * 60 * 60;
-            const token = sign({ id: user?._id, username, role: user?.role }, jwtSecretLive, {
-              expiresIn: maxAge, // 3hrs in sec
-            });
-            res.cookie('jwt', token, {
-              httpOnly: true,
-              maxAge: maxAge * 1000, // 3hrs in ms
-            });
-            res.status(201).json({
-              message: 'User successfully Logged in',
-              user: user?._id,
-            });
-          } else {
-            res.status(400).json({ message: 'Login not succesful' });
-          }
-        });
-      } else {
-        res.status(400).json({ message: 'Password missing or wrong. Login not succesful' });
-      }
-    });
+    const user = await User.findOne({ username });
+    if (user && (await compare(password, user.password))) {
+      const maxAge = 3 * 60 * 60;
+      const token = sign({ id: user._id, username, role: user.role }, jwtSecretLive, {
+        expiresIn: maxAge,
+      });
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+      res.status(200).json({ message: 'Successfully logged in', user: user._id });
+    } else {
+      res.status(400).json({ message: 'Invalid username or password' });
+    }
   } catch (error) {
     console.error(error);
-    return res.status(400).json({ message: 'Login failed' });
+    res.status(400).json({ message: 'Login failed' });
   }
 };
-
 export const update = async (req: Request, res: Response) => {
   const { role, id } = req.body;
   if (!role || !id) {
